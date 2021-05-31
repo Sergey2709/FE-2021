9ba92422aa2dc1f56281eae4326971ed39222339
Sergey2709 <33263150+Sergey2709@users.noreply.github.com>
2021-05-31 17:17:56 +0300

    31/05/21-4

--- JS/DOM/task2/index.js
+++ JS/DOM/task2/index.js
@@ -1,13 +1,12 @@
-const getTitleElement = titleElements => {
-    const titleElem = document.querySelector('.title');
-    console.dir(titleElem);
-}
+export const getTitleElement = titleElements => {
+  const titleElem = document.querySelector('.title');
+  console.dir(titleElem);
+};
 
-const getInputElement = inputElements => {
-    const inputElem = document.querySelector('.text');
-    console.dir(inputElem);
-}
-
+export const getInputElement = inputElements => {
+  const inputElem = document.querySelector('.text');
+  console.dir(inputElem);
+};
 
 getTitleElement();
 getInputElement();
