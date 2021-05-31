543a59027663362ea5e2a99badcd4d1901056903
Sergey2709 <33263150+Sergey2709@users.noreply.github.com>
2021-05-31 18:14:52 +0300

    31/05/21-6

---
+++ JS/DOM/task3/index.js
@@ -0,0 +1,11 @@
+export const getItemsList = listItems => {
+  const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
+};
+
+export const getItemsArray = listItems => {
+  const elementsList = document.querySelectorAll('.tool');
+  const elementsArray = Array.from(elementsList);
+  console.dir(elementsArray);
+};
+
