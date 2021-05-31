413bf40d2b69dc81d9c439f8c5673b47c6398d9b
Sergey2709 <33263150+Sergey2709@users.noreply.github.com>
2021-05-31 17:16:27 +0300

    31/05/21-4

---
+++ JS/DOM/task2/index.js
@@ -0,0 +1,13 @@
+const getTitleElement = titleElements => {
+    const titleElem = document.querySelector('.title');
+    console.dir(titleElem);
+}
+
+const getInputElement = inputElements => {
+    const inputElem = document.querySelector('.text');
+    console.dir(inputElem);
+}
+
+
+getTitleElement();
+getInputElement();
