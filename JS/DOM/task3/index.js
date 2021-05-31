87ca6e7c6564012f124651f0353ac56b86c43120
Sergey2709 <33263150+Sergey2709@users.noreply.github.com>
2021-05-31 19:31:13 +0300

    31/05/21-7

--- JS/DOM/task3/index.js
+++ JS/DOM/task3/index.js
@@ -1,13 +1,15 @@
 export const getItemsList = listItems => {
   const elementsList = document.querySelectorAll('.technology');
+  console.dir(elementsList);
   return elementsList;
 };
 
 export const getItemsArray = listItems => {
   const elementsList = document.querySelectorAll('.tool');
   const elementsArray = [...elementsList];
+  console.dir(elementsArray);
   return elementsArray;
 };
 
-// getItemsList();
-// getItemsArray();
+getItemsList();
+getItemsArray();
