2a238b1bd5d3fc66b5a1a2a00866fb30d28085ca
Sergey2709 <33263150+Sergey2709@users.noreply.github.com>
2021-05-31 17:36:32 +0300

    31/05/21-4

--- JS/DOM/task2/index.js
+++ JS/DOM/task2/index.js
@@ -1,10 +1,10 @@
-const getTitleElement = titleElements => {
+export const getTitleElement = titleElements => {
   const titleElem = document.querySelector('.title');
   console.dir(titleElem);
   return titleElem;
 };
 
-const getInputElement = inputElements => {
+export const getInputElement = inputElements => {
   const inputElem = document.getElementsByTagName('input', type='text');
   console.dir(inputElem);
   return inputElem;
